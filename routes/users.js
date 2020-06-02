9912a734b4e6ffb1d118ae932512b01f8e91d8ac
bosundare <bosun_tp2@yahoo.com>
2020-06-02 17:06:10 -0400

    Added password change

--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,7 @@ const User = require('../models/Users');
 const session = require('express-session');
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 const {userValidationRules} = require('../config/validation');
+const { check, validationResult } = require('express-validator/check');
 
 // Gets the login page
 router.get('/login', forwardAuthenticated, function(req, res, next){
@@ -74,6 +75,63 @@ router.post('/register', ensureAuthenticated, userValidationRules(), (req,res) =
   }
 });
 
+//Change Password Page
+router.get('/passwd', ensureAuthenticated, function(req, res){
+  res.render('changepwd', {user:req.user.username});
+  
+});
+
+//Change Password 
+router.post('/passwd', ensureAuthenticated, 
+[
+   check('password')
+  .isLength({min:5}).trim().withMessage('Minimum 5 Character Password')
+  .custom((value,{req}) => {
+   if (value !== req.body.password2) {
+       // throw error if passwords do not match
+       throw new Error("Passwords don't match");
+   } else {
+       return value;
+   }
+}),
+  ], (req, res)=>{
+  const {username, password,password2} = req.body;
+  let errors = req.validationErrors();
+  if (errors) {
+    console.log(errors);
+       res.render('changepwd',
+        { 
+         errors: errors, 
+         user: req.user.username,
+         username,
+         password,
+         password2
+        });
+     } else {
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(req.body.password, salt, (err, hash) => {
+          if (err) throw err;
+          req.body.password = hash;
+          User.findOne({where: {username: req.user.username}})
+          .then(users =>{
+            users.update({
+              password:req.body.password
+              })
+              .then(user => {
+              req.flash('success_msg','Password successfully updated');
+              res.redirect('/users/passwd');
+            }).catch(user => {
+      
+              req.flash('error_msg','Update failed, Check input');
+              res.redirect('/users/passwd');
+        })
+        })
+         .catch(err => console.log(err));
+        });
+      });
+     }
+
+})
     // Login Process
     router.post('/login', (req, res, next) => {
         passport.authenticate('local', {
