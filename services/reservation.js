4ba409944b124120c529f3de3b77d2d06c2d61c7
bosundare <bosun_tp2@yahoo.com>
2020-07-26 01:12:52 -0400

    ssh working

--- services/reservation.js
+++ services/reservation.js
@@ -3,13 +3,16 @@ const moment = require('moment-timezone')
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 const Cluster = require('../models/cluster');
+const config = require('../config/secret');
+let ssh = require('../config/newssh');
+const logger = require('../config/logger')
 
 setInterval(() => {
     start()
-}, 5000)
+}, 20000)
 
 const start = () => {
-Reservation.findOne({
+Reservation.findAll({
     where: {
         startDate: {[Op.lt]: moment().tz('America/New_York').format('YYYY-MM-DD HH:mm:ss')},
         status: {[Op.eq]: 'notapplied'}
@@ -18,19 +21,69 @@ Reservation.findOne({
 })
 .then(
    reservation => {
-       if (reservation) {
-           console.log('Clustername '+reservation.cluster.clustername)
-           reservation.update(
-               {
-                   status: 'error'
-               }
-           )
-       }
-       else {
-           console.log('No reservation found')
-       }
-   } 
+       reservation.forEach(reservation => {
+        if (reservation) {
+            let runcomm1 = [
+                "en",
+                "conf t",
+                "end",
+                "exit"
+                 ]
+                let tor1Options = {
+                ip: reservation.cluster.tor1ip,
+                username: config.switchadmin,
+                password: config.switchpass,
+                command: runcomm1
+            }
+            ssh.command(tor1Options, (err,data) => 
+    {if (err) {
+      logger.crit(err+ ' for '+reservation.cluster.clustername + ' for ' + 'Reservation ID ' + reservation.id)
+      reservation.update(
+        {
+            status: 'error'
+        }
+    )
+      } 
+      if (!err) {
+        logger.info("Changed TOR1 Configs "+reservation.cluster.tor1ip + ' for' + reservation.id)
+        let runcomm2 = [
+            "en",
+            "conf t",
+            "end",
+            "exit",
+       ]
+ 
+        let tor2Options = {
+            ip: reservation.cluster.tor2ip,
+            username: config.switchadmin,
+            password: config.switchpass,
+            command: runcomm2
+        }
+        
+        ssh.command(tor2Options, (err,data) => 
+        {if (err) {
+            logger.crit(err)
+            reservation.update(
+                {
+                    status: 'error'
+                }
+            )
+          // console.log(err)
+        } 
+          if(!err) 
+                {
+                  logger.info('Added Vlan '+reservation.extravlan + ' to ' + reservation.cluster.clustername)
+                  reservation.update(
+                    {
+                        status: 'applied'
+                    }
+                )
+                  }
+        })
+        
+    }
+                        } 
 )
-
-
-}
+        
+        }
+    })})}
