6b74446e28b071cac20fd7c26948f606275fa145
bosundare <bosun_tp2@yahoo.com>
2020-08-13 11:47:28 -0400

    Fixed sync issue

--- services/alerts.js
+++ services/alerts.js
@@ -15,21 +15,13 @@ axios.defaults.headers.common ['Authorization'] = config.axiosauth;
 const service = () => {
     setInterval(() => {
         start()
-    },6 * 60 * 60 * 1000)
+    },2 * 60 * 60 * 1000)
 
     const start = async () => {
       
       try {
         await Alert.destroy({where: {},truncate: true}).then(console.log('Table cleared'));
-        await Cluster.findAll({
-          where: { 
-            [Op.or]: [
-            {
-              clustername: {[Op.like]: '%%'}
-            },
-            ]
-          }
-      })
+        await Cluster.findAll({})
         .then(
           cluster => {
             cluster.forEach(
@@ -58,19 +50,19 @@ const service = () => {
         .then(
            alert => {
             alert.forEach(
-               async alert => {
+               alert => {
               
               let cip = alert.cluster.clusterip
               let alerttotalnum = 0
               let alertmessage = []
     
-             await axios.all([
+             axios.all([
               axios.get('https://'+cip+':9440/PrismGateway/services/rest/v2.0/alerts/?resolved=false&alert_type_uuid=A1082', {timeout: 5000}),
               axios.get('https://'+cip+':9440/PrismGateway/services/rest/v2.0/alerts/?resolved=false&alert_type_uuid=A15030', {timeout: 5000}),
               axios.get('https://'+cip+':9440/PrismGateway/services/rest/v2.0/alerts/?resolved=false&alert_type_uuid=A101048', {timeout: 5000}),
               axios.get('https://'+cip+':9440/PrismGateway/services/rest/v2.0/alerts/?resolved=false&alert_type_uuid=A1069', {timeout: 5000}),
               axios.get('https://'+cip+':9440/PrismGateway/services/rest/v2.0/alerts/?resolved=false&alert_type_uuid=1165', {timeout: 5000}),
-              axios.get('https://'+cip+':9440/PrismGateway/services/rest/v2.0/alerts/?resolved=false&alert_type_uuid=A1050', {timeout: 5000}),
+              axios.get('https://'+cip+':9440/PrismGateway/services/rest/v2.0/alerts/?resolved=false&alert_type_uuid=A106051', {timeout: 5000}),
               axios.get('https://'+cip+':9440/PrismGateway/services/rest/v2.0/alerts/?resolved=false&alert_type_uuid=A1187', {timeout: 5000}),
               axios.get('https://'+cip+':9440/PrismGateway/services/rest/v2.0/alerts/?resolved=false&alert_type_uuid=A106017', {timeout: 5000}),
               axios.get('https://'+cip+':9440/PrismGateway/services/rest/v2.0/alerts/?resolved=false&alert_type_uuid=A106053', {timeout: 5000}),
@@ -78,8 +70,8 @@ const service = () => {
               axios.get('https://'+cip+':9440/PrismGateway/services/rest/v2.0/alerts/?resolved=false&alert_type_uuid=A15039', {timeout: 5000})
               
             ])
-              .then(axios.spread((nic,ipmi,offlinedisk,storagehealth,satadom,rcmperror,dimmeccerror,smsgdrive,boordrive,nodefail,dimmuccerror) => {
-                let axiosresponses = [nic,ipmi,offlinedisk,storagehealth,satadom,rcmperror,dimmeccerror,smsgdrive,boordrive,nodefail,dimmuccerror]
+              .then(axios.spread((nic,ipmi,offlinedisk,storagehealth,satadom,pm883err,dimmeccerror,smsgdrive,boordrive,nodefail,dimmuccerror) => {
+                let axiosresponses = [nic,ipmi,offlinedisk,storagehealth,satadom,pm883err,dimmeccerror,smsgdrive,boordrive,nodefail,dimmuccerror]
                 axiosresponses.forEach(
                   axiosres => {
                     let alertnum = axiosres.data.metadata.total_entities
